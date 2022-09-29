175bfff2e5684c48dd6bab751f064afddb941b24
Mona Lisa <nasiromar321@gmail.com>
2022-09-29 12:15:14 -0500

    function

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,31 @@
 let beginEl = document.getElementById ("begin-game");
 beginEl.addEventListener("click", beginGame);
-timeEl = document.getElementById("timer");
+let timeEl = document.getElementById("timer");
 let timerId; 
 let timeLeft = 120;
+let bcEl = document.querySelector(".beginning-container");
+let qcEl = document.querySelector("#question-container");
+
+
 function beginGame (){
     console.log("beginGame button clicked")
     timerId = setInterval (timerFunc, 1000);
     timeEl.textContent = timeLeft;
-        
-    
+    bcEl.removeAttribute("class", "show");
+    bcEl.setAttribute("class", "hide");    
+    qcEl.removeAttribute("class", "hide");  
 }
 
 function timerFunc() {
     timeLeft--;
     timeEl.textContent = timeLeft;
+
+    if (timeLeft<= 0 ) {
+        endGame ();
+
+    }
+
 }
+ function endGame () {
+    // tbd 
+ }
