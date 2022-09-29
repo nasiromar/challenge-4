ac71dedcdd8c8db71ab144a7e6b5bee1d0410ce5
Mona Lisa <nasiromar321@gmail.com>
2022-09-28 21:32:05 -0500

    timer works

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,17 @@
 let beginEl = document.getElementById ("begin-game");
 beginEl.addEventListener("click", beginGame);
-
+timeEl = document.getElementById("timer");
+let timerId; 
+let timeLeft = 120;
 function beginGame (){
     console.log("beginGame button clicked")
+    timerId = setInterval (timerFunc, 1000);
+    timeEl.textContent = timeLeft;
+        
+    
+}
+
+function timerFunc() {
+    timeLeft--;
+    timeEl.textContent = timeLeft;
 }
